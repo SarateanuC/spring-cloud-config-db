019e27612b92c00332d0bff9021db66e3b8bf982
SarateanuC <cojocaru.carolina@mail.globaldatabase.com>
2023-02-09 14:01:20 +0200

    init project

---
+++ src/main/java/com/example/springcloudconfigdb/SpringCloudConfigDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigDbApplication.class, args);
+    }
+
+}
